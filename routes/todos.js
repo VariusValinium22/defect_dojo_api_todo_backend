c8e131320f7bc5d425de91136d8ae776c986d525
Martin Young <MartingYoung@hotmail.com>
2025-01-26 20:11:14 -0600

    committing README.md file to deescribe the topics covered in the project

--- routes/todos.js
+++ routes/todos.js
@@ -7,19 +7,19 @@ let todos = [
     { id: 2, text: 'take a nap'},
 ];
 
-// GET todos
+// GET todos API endpoint
 router.get('/', (req, res) => {
     res.json(todos);
 });
 
-// POST todos
+// POST todos API endpoint
 router.post('/', (req, res) => {
     const newTodo = { id: Date.now(), text: req.body.text };
     todos.push(newTodo);
     res.status(201).json(newTodo);
 });
 
-// DELETE todo by id
+// DELETE todo by id API endpoint
 router.delete('/:id', (req, res) => {
     const id = parseInt(req.params.id);
     todos = todos.filter((todo) => todo.id !== id);
